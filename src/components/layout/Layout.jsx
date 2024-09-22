66afe5d865f0aa49cb4dab7c101da38b9534af70
Ourania Ntrizi <drizirania@gmail.com>
2024-09-22 22:46:37 +0300

    vercel

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
 import { Analytics } from "@vercel/analytics/react"
+
 /* eslint-disable react/prop-types */
 const Layout = ({ children }) => {
     return (
@@ -10,7 +11,7 @@ const Layout = ({ children }) => {
                 {children}
             </div>
             <Footer />
-            <Analytics />
+            <Analytics debug={true} /> {/* This will log debug info to the console */}
         </div>
     );
 }
